3d67f714cadedd101a816c017f44d73a675406eb
arakiyuto <araki@pyxos-jk.co.jp>
2020-12-08 14:15:59 +0900

    create CompositionSample

---
+++ src/components/CompositionSample.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ fullName }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, ref } from "@vue/composition-api";
+interface User {
+  firstName: string;
+  lastName: string;
+}
+export default defineComponent({
+  props: {
+    user: {
+      type: Object as () => User,
+      required: true
+    }
+  },
+  setup({ user }) {
+    const fullName = computed(() => `${user.firstName} ${user.lastName}`);
+    return {
+      fullName
+    };
+  }
+});
+</script>
