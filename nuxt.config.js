26a0990719748961d1e52b1f204afd7763331497
arakiyuto <araki@pyxos-jk.co.jp>
2020-12-08 13:58:56 +0900

    fix path for src folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
+  srcDir: 'src',
   head: {
     titleTemplate: '%s - front',
     title: 'front',
