a7718f6f923f0a1c2926174f4dee1ff7a63f5adb
arakiyuto <araki@pyxos-jk.co.jp>
2020-12-08 14:08:22 +0900

    setting jest-ts

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,28 @@
 module.exports = {
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: [
     'js',
+    'ts',
     'vue',
     'json'
   ],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.tsx$': 'ts-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/src/components/**/*.vue',
+    '<rootDir>/src/pages/**/*.vue'
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/(?!@babel/runtime-corejs2)'
   ]
 }
